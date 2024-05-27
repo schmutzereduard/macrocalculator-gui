cf9a568367b6b8b41e82fe0e3ce34ac3677ee486
Schmutzer Eduard <schmutzer.eduard@gmail.com>
2024-05-27 16:44:02 +0300

    baby steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Foods from './components/Foods';
+import Meals from './components/Meals';
+import Planner from './components/Planner';
+import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route path="/foods" element={<Foods />} />
+                    <Route path="/meals" element={<Meals />} />
+                    <Route path="/planner" element={<Planner />} />
+                    <Route path="/calculator" element={<Calculator />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h2>Macro Calculator</h2>
+            <div className="nav-buttons">
+                <Link to="/foods" className="nav-button">Foods</Link>
+                <Link to="/meals" className="nav-button">Meals</Link>
+                <Link to="/planner" className="nav-button">Planner</Link>
+                <Link to="/calculator" className="nav-button">Calculator</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
